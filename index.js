6ccf0f34346f069d81add4677d2f230bb8202db5
Waite <waiteb@sea-waiteb-m1.ds.ad.adp.com>
2017-04-10 00:00:57 -0700

    Making attributes instanced instead of static

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Stencil = require("./lib/stencil.js");
 
 // Add all of the default expressions:
 Stencil.defaultExpressions = require("./lib/stencil.expressions.js");
+Stencil.defaultAttributes = require("./lib/stencil.attributes.js");
 
 module.exports = Stencil;
 
