1a1b8e4b06e219fb0ead59c9dd8c63b8019e397b
waiteb <b.waite@mac.com>
2017-04-09 22:59:26 -0700

    Refactored expressions to be instanced, rather than static

---
+++ index.js
@@ -0,0 +1,7 @@
+var Stencil = require("./lib/stencil.js");
+
+// Add all of the default expressions:
+Stencil.defaultExpressions = require("./lib/stencil.expressions.js");
+
+module.exports = Stencil;
+
