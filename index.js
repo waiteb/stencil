4fe3d06024c22e7f0b66edbf25cc009982c36b1a
waiteb <b.waite@mac.com>
2017-04-10 00:30:38 -0700

    Making custom elements instanced instead of static

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var Stencil = require("./lib/stencil.js");
 // Add all of the default expressions:
 Stencil.defaultExpressions = require("./lib/stencil.expressions.js");
 Stencil.defaultAttributes = require("./lib/stencil.attributes.js");
+Stencil.defaultElements = require("./lib/stencil.elements.js");
 
 module.exports = Stencil;
 
