d1acc3d23ec8be0f7848fa3aaedad5c1e84cdd74
Waite <waiteb@sea-waiteb-m1.ds.ad.adp.com>
2017-04-10 02:18:08 -0700

    Added substencils

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var Stencil = require("./lib/stencil.js");
 Stencil.defaultExpressions = require("./lib/stencil.expressions.js");
 Stencil.defaultAttributes = require("./lib/stencil.attributes.js");
 Stencil.defaultElements = require("./lib/stencil.elements.js");
+Stencil.defaultSubstencils = {
+    undefined: new Stencil('<!-- missing substencil: {{name}} -->')
+};
 
 module.exports = Stencil;
-
