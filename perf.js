e923c55dd55cf8ce3080da44007710d1882bb5ab
waiteb <b.waite@mac.com>
2017-04-14 20:22:41 -0700

    stencil.renderChildren performance enhancements

--- perf.js
+++ perf.js
@@ -1,9 +1,11 @@
 /* jshint esversion: 6 */
 var Stencil = require("./index");
 var html = `
-    <div alpha="123" beta="123" gamma="123">
-        <div alpha="123" beta="123" gamma="123">
-            <div alpha="123" beta="123" gamma="123"></div>
+    <div alpha="{{alpha.value}}" beta="{{alpha.beta.value}}">
+        <div alpha="{{alpha.value}}" beta="{{alpha.beta.value}}">
+            <div alpha="{{alpha.value}}" beta="{{alpha.beta.value}}">
+                <div each="alpha.beta.gamma">{{this}}</div>
+            </div>
         </div>
      </div>
 `;
@@ -11,10 +13,20 @@ var times = 1024 * 8;
 
 var stencil = new Stencil(html);
 
-console.profile("stencil.render");
+var model = {
+    alpha: {
+        value: "alpha",
+        beta: {
+            value: "beta",
+            gamma: [0, 2, 4, 8, 16, 32, 64, 128]
+        }
+    }    
+};
+
+if (console.profile) {console.profile("stencil.render");}
 console.time("x" + times);
 for (let i = 0, n = times; i < n; i++) {
-    let html = stencil.render();
+    let html = stencil.render(model);
 }
 console.timeEnd("x" + times);
-console.profileEnd("stencil.render");
+if (console.profile) {console.profileEnd("stencil.render");}
